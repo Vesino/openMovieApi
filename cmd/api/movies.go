444ff1015b69a22ef14902b946c8e7d403df0d5a
Luis Vargas <luisvargasr10@gmail.com>
2022-05-20 19:12:16 -0500

    Json responses with with struct tags in structs

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/Vesino/openMovieApi/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +18,19 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		http.NotFound(w, r)
 		return
 	}
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Santo contra las momias",
+		Year:      1994,
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Printf(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
---
+++ internal/data/movies.go
@@ -0,0 +1,15 @@
+package data
+
+import "time"
+
+// Annotate the Movie struct with struct tags to control how the keys appear in the
+// JSON-encoded output.
+type Movie struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"` // The - (hyphen) directive can be used when you never want a particular struct field to appear in the JSON output.
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`    // Add the omitempty directive
+	Runtime   int32     `json:"runtime,omitempty"` // Add the omitempty directive
+	Genres    []string  `json:"genres,omitempty"`  // Add the omitempty directive
+	Version   int32     `json:"version"`
+}
