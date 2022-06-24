7ac31f88ca86f5bf3f5ef4e8b4a858daf9767087
Luis Vargas <luisvargasr10@gmail.com>
2022-06-24 00:11:32 -0500

    Handling race condition with optimistic lock approach

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -125,7 +125,12 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 	// Pass the updated movie record to our new Update() method.
 	err = app.models.Movies.Update(movie)
 	if err != nil {
-		app.serverErrorResponse(w, r, err)
+		switch {
+		case errors.Is(err, data.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
 		return
 	}
 
@@ -185,7 +190,12 @@ func (app *application) partialUpdateMovieHandler(w http.ResponseWriter, r *http
 	}
 	err = app.models.Movies.Update(movie)
 	if err != nil {
-		app.serverErrorResponse(w, r, err)
+		switch {
+		case errors.Is(err, data.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
 		return
 	}
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
