be610a3cfbf07e8f6e889a411d4e05df7e1607cc
Luis Vargas <luisvargasr10@gmail.com>
2022-07-06 23:53:11 -0500

    Create new endpoint listMoviesHandler, creation of helper functions to get query params

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -17,6 +17,8 @@ func (app *application) routes() *httprouter.Router {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.updateMovieHandler)
