d37118b75b84a5235565f2a7beecdf7dba9842b3
Luis Vargas <luisvargasr10@gmail.com>
2022-07-28 08:58:56 -0500

    GetAll SQL method for list movieHAndler

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -15,6 +15,7 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.Page <= 10_000_000, "page", "must be a maximum of 10 million")
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
+
 	// Check that the sort parameter matches a value in the safelist.
 	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
 }
